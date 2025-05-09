25be943db0421a2ed4dc6d8d910cfd58bf49de38
MartijnVanhengelPXL <148761992+MartijnVanhengelPXL@users.noreply.github.com>
2025-05-09 19:42:52 +0200

    eindelijk klaar

--- backend/index.js
+++ backend/index.js
@@ -49,14 +49,22 @@ async function setUserPassword() {
 async function updatePasswords() {
     try {
         const result = await pool.query('SELECT id, user_name, password FROM users');
+
         for (let user of result.rows) {
-            const hashedPassword = await bcrypt.hash(user.password, 10);
-            await pool.query('UPDATE users SET password = $1 WHERE id = $2', [hashedPassword, user.id]);
+            if (!user.password.startsWith('$2b$')) { 
+                const hashedPassword = await bcrypt.hash(user.password, 10);
+
+                await pool.query(
+                    'UPDATE users SET password = $1 WHERE id = $2',
+                    [hashedPassword, user.id]
+                );
+            }
         }
     } catch (err) {
         console.error('Fout bij het updaten van wachtwoorden:', err);
     }
 }
+
 
 async function createDefaultUsers() {
     const users = [
